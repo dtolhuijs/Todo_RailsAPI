50d87a3b86bb993b6015f9d5893b9d09184348af
dtolhuijs <deisi.bonilla@gmail.com>
2016-07-06 20:37:18 +0200

    added spec for routing controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'todos' => 'todos#index'
-  
+  get 'todos', to: 'todos#show'
+
+  resources :todos, only: [:show, :index]
+
   root 'todos#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
