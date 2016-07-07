1da90c798262d59a7e5487ab947f1015e5069470
dtolhuijs <deisi.bonilla@gmail.com>
2016-07-07 22:56:37 +0200

    added task and edit functions to todo app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CreateTodo from './CreateTodo';
 import TodosList from './TodosList';
 
 const todos = [
@@ -29,11 +30,42 @@ class App extends React.Component {
     };
   }
 
+  toggleTask(task) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task === task);
+    foundTodo.isCompleted = !foundTodo.isCompleted;
+    this.setState({todos: this.state.todos});
+  }
+
+  createTask(task) {
+    this.state.todos.push({
+      task,
+      isCompleted: false
+    });
+    this.setState({todos: this.state.todos });
+  }
+
+  saveTask(oldTask, newTask) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
+    foundTodo.task = newTask;
+    this.setState({ todos: this.state.todos });
+  }
+
+  deleteTask(taskToDelete) {
+    _.remove(this.state.todos, todo => todo.task === taskToDelete);
+    this.setState({ todos: this.state.todos });
+  }
+
     render() {
         return (
           <div>
             <h1>Todo List</h1>
-            <TodosList todos={this.state.todos}/>
+            <CreateTodo todos={this.state.todos} createTask={this.createTask.bind(this)}/>
+            <TodosList
+            todos={this.state.todos}
+            toggleTask={this.toggleTask.bind(this)}
+            saveTask={this.saveTask.bind(this)}
+            deleteTask={this.deleteTask.bind(this)}
+            />
           </div>
         );
     }
--- src/TodosList.js
+++ src/TodosList.js
@@ -1,23 +1,38 @@
 import _ from 'lodash';
+import jQuery from 'jquery';
 import React from 'react';
 import TodosListHeader from './TodosListHeader';
 import TodosListItem from './TodosListItem';
 
   class TodosList extends React.Component {
-    renderItems(){
-      return _.map(this.props.todos, (todo, index) => <TodosListItem key={index
-      } {...todo} />);
+    constructor(){
+      super();
+
+      this.state = {
+        message: "There are no task yet.",
+        todos: []
+      };
     }
-      render() {
-          return (
-              <table>
-                <TodosListHeader />
-                  <tbody>
-                    {this.renderItems()}
-                  </tbody>
-              </table>
-          );
-      }
+
+    renderItems(){
+      const props = _.omit(this.props, 'todos');
+
+      return _.map(this.props.todos,
+        (todo, index) => <TodosListItem key={index}
+        {...todo} {...props} />
+      );
+    }
+
+    render() {
+      return (
+        <table>
+          <TodosListHeader />
+          <tbody>
+            {this.renderItems()}
+          </tbody>
+        </table>
+      );
+    }
   }
 
 export default TodosList;
